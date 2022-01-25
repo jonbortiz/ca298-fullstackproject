cd35d0a5f8368030ad09a7760a929c48c53d1222
Jon Bryan Ortiz <jonbryanortiz01@gmail.com>
2022-01-25 15:42:37 +0000

    Added item models, base & index html files. Also linked urls. Setup superadmin account

--- backend/jonshop/admin.py
+++ backend/jonshop/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import APIUser
+from .models import *
 
 admin.site.register(APIUser, UserAdmin)
+admin.site.register(Product)
+admin.site.register(Basket)
+admin.site.register(BasketItems)
+admin.site.register(Order)
