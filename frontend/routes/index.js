680fc64ffb53d8a6034fd52fd843fdaaa52451bf
Jon Bryan Ortiz <jonbryanortiz01@gmail.com>
2022-03-24 11:13:01 +0000

    Added Frontend

---
+++ frontend/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next){
+  res.render('login')
+});
+
+router.get('/productindividual', function(req, res, next){
+  res.render('productindividual')
+});
+
+router.get('/basket', function(req, res, next){
+  res.render('basket')
+});
+
+router.get('/footer', function(req, res, next) {
+  res.render('footer')
+});
+
+router.get('/header', function(req, res, next) {
+  res.render('header')
+});
+
+module.exports = router;
